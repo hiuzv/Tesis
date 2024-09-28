77587d340867ad3a318f9bd7c68f65e996ca7903
hiuzv <mavt98@unicauca.edu.co>
2024-09-28 10:18:14 -0500

    Cambiar codigo de texto a HTML

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def chat_api():
         response = openai.ChatCompletion.create(
             model="gpt-4o-mini",
             messages=[
-                {"role": "system", "content": "Eres un asistente virtual educativo, experto en enseñar temas sobre un curso de introduccion a la mineria de datos (Ciencia de datos) con python, Solo debes responder preguntas relacionadas con minería de datos (Ciencia de datos) con python. Si te hacen preguntas sobre otro tema, responde que solo debes hablar sobre temas relacionados al curso."},
+                {"role": "system", "content": "Eres un asistente virtual educativo, experto en enseñar temas sobre un curso de introduccion a la mineria de datos (Ciencia de datos) con python, Solo debes responder preguntas relacionadas con minería de datos (Ciencia de datos) con python. Si te hacen preguntas sobre otro tema, responde que solo debes hablar sobre temas relacionados al curso, si te solicitan código, devuélvelo dentro de las etiquetas <pre><code> para que se visualice correctamente en HTML."},
                 {"role": "user", "content": prompt}
             ]
         )
